6ac7d0a188f52a9fef646bbdb45505d60979ad3c
nominik <mike.nogai.spb@mail.ru>
2025-07-20 22:59:22 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,47 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        // Заполнение дерева
+        // Названия переменных указывают на место заполняемого узла
+        // например, rl - повернуть на право, затем налево
+        // После заполнения дерево выводится на консоль, можете ориентироваться на него
+
+        Tree ll = new Tree("Александа");
+        Tree lr = new Tree("Владимир");
+        Tree l = new Tree("Борис");
+        l.setLeft(ll);
+        l.setRight(lr);
+
+        Tree rl = new Tree("Иннокентий");
+        Tree rr = new Tree("Пантелеймон");
+        Tree r = new Tree("Константин");
+        r.setLeft(rl);
+        r.setRight(rr);
+
+        Tree root = new Tree("Зоя");
+        root.setLeft(l);
+        root.setRight(r);
+
+        System.out.println(root); // Выведем дерево в консоль
+
+        System.out.println("Проверка поиска по дереву:");
+        System.out.println(root.contains("Иннокентий")); // true
+        System.out.println(root.contains("Борис")); // true
+        System.out.println(root.contains("Анна")); // false
+
+        // Ваше задание (нужно раскомментировать)
+        System.out.println("Проверка на пирамидальность по длине имени");
+
+        System.out.println(root.isNamePyramid()); // true
+
+        // Меняем имя в одном из узлов на Павел
+        // Пирамидальность должна нарушиться
+        // А из-за того что имя на ту же букву,
+        // в данном случае свойства дерева поиска сохрнаяются
+        rr.setName("Павел");
+        System.out.println(root.isNamePyramid()); // false
     }
+
 }
